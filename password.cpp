9b8e8fbf1885d2c5e717e826e094de269ecde6d4
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-01-24 16:05:00 -0500

    Create password.cpp

---
+++ password.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string input = "";
+    
+    //Start your loop here
+    
+    cout << "Enter password: ";
+    cin >> input;
+    
+
+}
